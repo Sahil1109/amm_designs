fb3ab87815bcf4aef27fdfd432712b43ec17f80a
Sahil1109 <sahil.jangra@block8.com>
2020-09-01 12:33:43 +0530

    Add Number format

--- src/Components/TextFieldAmm.tsx
+++ src/Components/TextFieldAmm.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import StyledField, { fieldStyles } from "./StyledField";
 import LockIcon from "@material-ui/icons/Lock";
+import NumberFormat from "react-number-format";
+import { styled } from "@material-ui/core";
 export default function TextFieldAmm() {
   const classes = fieldStyles();
   return (
     <div>
       <StyledField
         placeholder="0.00"
-        endAdornment={
-          <InputAdornment position="start" style={{marginRight:"250px"}}>
-            {/* <LockIcon></LockIcon>{" "} */}%
-          </InputAdornment>
-        }
+        // endAdornment={
+        //   <InputAdornment position="start" style={{marginRight:"250px"}}>
+        //     {/* <LockIcon></LockIcon>{" "} */}%
+        //   </InputAdornment>
+        // }
       ></StyledField>
       <br></br>
-      <StyledField
-        placeholder="000 000"
-        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-      ></StyledField>
+      <NumberFormat
+        customInput={StyledField}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <br></br>
+      <NumberFormat customInput={StyledField} suffix={"%"} />
       <br></br>
       <StyledField
         placeholder="000 000"
